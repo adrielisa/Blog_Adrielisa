ebbece2c03fece8f4ca72f65db4b2a97b9a65d52
Adriel Rodriguez <danielganer67@gmail.com>
2025-01-08 12:47:17 -0500

    .gitignore and backend update
    gitignore of .env and requirements.txt

---
+++ Backend/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.database import engine, Base
+from app.routers import users, posts, comments
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+#Routes included
+app.include_router(users.router)
+app.include_router(posts.router)
+app.include_router(comments.router)
+
+@app.get("/")
+def read_root():
+    return {"message": "¡Welcome to Adrielisa's blog backend!"}
